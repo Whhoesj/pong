2baab0dc870afb4a012c33795604fbdf51c50c5c
Wouter Habets <wouterhabets@gmail.com>
2015-04-21 17:41:48 +0200

    Added multiplayer support

---
+++ PongAndroid/libpong/src/main/java/nl/sjtek/smartmobile/libpong/ui/AsyncTaskClient.java
@@ -0,0 +1,121 @@
+package nl.sjtek.smartmobile.libpong.ui;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import nl.sjtek.smartmobile.libpong.game.GameState;
+import nl.sjtek.smartmobile.libpong.game.MovementUpdate;
+
+/**
+ * An {@link android.os.AsyncTask} to connect to an Pong host. ({@link nl.sjtek.smartmobile.libpong.ui.AsyncTaskHost})
+ * <p>
+ *     This {@link android.os.AsyncTask} will connect to a Pong host. The constructor takes the connection parameters.<br>
+ *     Bat movement can be send with {@link AsyncTaskClient#sendMovementUpdate(nl.sjtek.smartmobile.libpong.game.MovementUpdate)}.<br>
+ *     The latest received {@link nl.sjtek.smartmobile.libpong.game.GameState} can be requested with {@link AsyncTaskClient#getGameState()}.
+ * </p>
+ */
+public class AsyncTaskClient extends AsyncTask<Void, Void, Void> {
+
+    private final String serverAddress;
+    private final int serverPort;
+
+    private GameState gameState;
+    private MovementUpdate movementUpdate;
+    private boolean running;
+
+    /**
+     * Setup the {@link android.os.AsyncTask}.
+     * @param serverAddress The address to connect to
+     * @param serverPort The port to connect to
+     */
+    public AsyncTaskClient(String serverAddress, int serverPort) {
+        this.serverAddress = serverAddress;
+        this.serverPort = serverPort;
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+
+        StateReceiverThread stateReceiverThread;
+        MovementSenderThread movementSenderThread;
+
+        try {
+            stateReceiverThread = new StateReceiverThread(
+                    new Socket(serverAddress, serverPort));
+            movementSenderThread = new MovementSenderThread(
+                    new Socket(serverAddress, serverPort));
+
+            stateReceiverThread.run();
+            movementSenderThread.run();
+
+            while (running);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public void sendMovementUpdate(MovementUpdate movementUpdate) {
+        this.movementUpdate = movementUpdate;
+    }
+
+    private class MovementSenderThread implements Runnable {
+
+        private Socket socket;
+
+        private MovementSenderThread(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                ObjectOutputStream objectOutputStream =
+                        new ObjectOutputStream(socket.getOutputStream());
+
+                while (running) {
+                    objectOutputStream.writeObject(movementUpdate);
+                    Thread.sleep(50);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class StateReceiverThread implements Runnable {
+
+        private Socket socket;
+
+        private StateReceiverThread(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                ObjectInputStream objectInputStream =
+                        new ObjectInputStream(socket.getInputStream());
+
+                while (running) {
+                    gameState = (GameState) objectInputStream.readObject();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ PongAndroid/libpong/src/main/java/nl/sjtek/smartmobile/libpong/ui/AsyncTaskHost.java
@@ -0,0 +1,125 @@
+package nl.sjtek.smartmobile.libpong.ui;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import nl.sjtek.smartmobile.libpong.game.GameState;
+import nl.sjtek.smartmobile.libpong.game.MovementUpdate;
+
+/**
+ * An {@link android.os.AsyncTask} to host a game of Pong.
+ * <p>
+ *     This {@link android.os.AsyncTask} will listen for a player on port 1337 when executed.<br>
+ *     When running, an {@link nl.sjtek.smartmobile.libpong.game.GameState} can be send with
+ *     {@link nl.sjtek.smartmobile.libpong.ui.AsyncTaskHost#sendGameState(nl.sjtek.smartmobile.libpong.game.GameState)}.
+ * </p>
+ */
+public class AsyncTaskHost extends AsyncTask<Void, Void, Void> {
+
+    /**
+     * The port the host will listen to.
+     */
+    public static final int PORT = 1337;
+
+    private boolean running = true;
+
+    private GameState gameState;
+    private MovementUpdate movementUpdate;
+
+    /**
+     * Send a new {@link nl.sjtek.smartmobile.libpong.game.GameState} to the client.
+     * @param gameState The GameState to send
+     */
+    public void sendGameState(GameState gameState) {
+        this.gameState = gameState;
+    }
+
+    /**
+     * Get the latest received {@link nl.sjtek.smartmobile.libpong.game.MovementUpdate}.
+     * @return Latest received {@link nl.sjtek.smartmobile.libpong.game.MovementUpdate}
+     */
+    public MovementUpdate getMovementUpdate() {
+        return movementUpdate;
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+
+        ServerSocket serverSocket;
+        StateUpdaterThread stateUpdaterThread;
+        MovementReceiverThread movementReceiverThread;
+
+        try {
+            serverSocket = new ServerSocket(PORT);
+            stateUpdaterThread = new StateUpdaterThread(serverSocket.accept());
+            movementReceiverThread = new MovementReceiverThread(serverSocket.accept());
+            stateUpdaterThread.run();
+            movementReceiverThread.run();
+        } catch (IOException e) {
+            running = false;
+            e.printStackTrace();
+        }
+
+
+        while (running);
+
+        return null;
+    }
+
+    private class StateUpdaterThread implements Runnable {
+
+        private final Socket socket;
+
+        private StateUpdaterThread(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+
+                while (running) {
+                    objectOutputStream.writeObject(gameState);
+                    Thread.sleep(50);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                running = false;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class MovementReceiverThread implements Runnable {
+
+        private final Socket socket;
+
+        private MovementReceiverThread(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+
+                while (running) {
+                    movementUpdate = (MovementUpdate) objectInputStream.readObject();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
