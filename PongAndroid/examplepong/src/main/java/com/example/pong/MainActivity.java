6559cc79fbb4dd03ebb9c3c32924173a54d5cc67
Wouter Habets <wouterhabets@gmail.com>
2015-04-23 13:14:59 +0200

    Added example

---
+++ PongAndroid/examplepong/src/main/java/com/example/pong/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.pong;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import nl.sjtek.smartmobile.libpong.ui.PongView;
+import nl.sjtek.smartmobile.libpong.ui.Utils;
+
+
+public class MainActivity extends ActionBarActivity implements SensorEventListener {
+
+    private PongView pongView;
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private float previousValue = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pongView = (PongView) findViewById(R.id.pongView);
+        pongView.setSingleplayer();
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float mappedValue = Utils.map(sensorEvent.values[0], -2, 2, 0, 500);
+        float smoothValue = Utils.exponentialSmoothing(previousValue, mappedValue, 0.1f);
+        pongView.setBottomBatX((int) smoothValue);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
